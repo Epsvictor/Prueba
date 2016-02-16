6f4b91a0fa4726580c2324b11fcfcac8d4fadfa9
Epsvictor <vicluna003@gmail.com>
2016-02-16 11:04:56 -0600

    metodo get agregado, probando JSON

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,34 @@
 function doClick(e) {
-    alert($.label.text);
+    alert('succes');
 }
 
 $.index.open();
-
+	
+    var url = "http://www.tvazteca.com/historico/index/more/format/json?program=272&string=&category=0&type=2&page=-2&numItems=100&imgW=65&imgH=65";
+    var OL;
+   
+    var client = Ti.Network.createHTTPClient({
+  
+     onload : function(e) {
+     	 OL=JSON.parse(this.responseText);
+         Ti.API.info(this.responseText);
+         JSON.stringify(OL);
+         alert(JSON.stringify(OL));
+         
+         for (var i=0;i<OL.items.length;i++){
+         	
+         	
+         }
+     },
+     
+     onerror : function(e) {
+         Ti.API.debug(e.error);
+         alert('ERROR');
+     },
+     timeout : 5000 
+ });
+ 
+   client.open("GET", url);
+   client.send();
+ 
 
